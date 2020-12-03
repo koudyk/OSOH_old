e726c01c295d275472938f58f5cf4d382e68220a
Kendra Oudyk <kendra.oudyk@gmail.com>
2020-12-02 23:42:44 -0500

    start OLS presentation

---
+++ presentations/OLS_presentation_16-12-2020.py
@@ -0,0 +1,182 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# + slideshow={"slide_type": "skip"}
+
+
+
+# + [markdown] slideshow={"slide_type": "slide"}
+#
+# ![](images/logo_medium.png)
+#
+# Kendra Oudyk
+#
+# 2020-12-16 \
+# Final presentation for OLS-2
+#
+# <span style="color:gray">Twitter: @koudyk_ </span> \
+# <span style="color:gray">GitHub: koudyk/OSOH </span>
+#
+#
+#
+# + [markdown] slideshow={"slide_type": "slide"}
+# # Vision
+#
+# OSOH is an online space where grad students can get regular help making their research more open. 
+
+
+# + [markdown] slideshow={"slide_type": "subslide"}
+# #### inviting students to the
+# ## Open science buffet
+# ![](images/open_science_buffet.png)
+
+
+# + [markdown] slideshow={"slide_type": "subslide"}
+# # DEEEETAAAAILS
+
+# + [markdown] slideshow={"slide_type": "slide"}
+# # Milestones
+#
+# - **Evaluate needs**
+# - Create documentation and website
+# - Build network of collaborators
+# - Request funding
+
+# + [markdown] slideshow={"slide_type": "subslide"}
+# # Creating and sharing our survey
+# ![](images/survey_screenshot.png)
+
+# + [markdown] slideshow={"slide_type": "fragment"}
+# ![](images/survey_on_twitter.png)
+
+# + [markdown] slideshow={"slide_type": "subslide"}
+# # 27 participants so far
+
+# + [markdown] slideshow={"slide_type": "fragment"}
+# ![](../survey/figures/participants_background.png)
+
+# + [markdown] slideshow={"slide_type": "subslide"}
+# # What do you think of when you hear the words "open science"?
+# ![](../survey/figures/wc_first_thoughts.png)
+
+# + [markdown] slideshow={"slide_type": "subslide"}
+# ![](images/open_science_buffet.png)
+
+# + [markdown] slideshow={"slide_type": "subslide"}
+# # What's your experience *using* open-science objects?
+# ![](../survey/figures/experience_using.png)
+
+# + [markdown] slideshow={"slide_type": "subslide"}
+# # What's your experience *creating* open-science objects?
+#
+# ![](../survey/figures/experience_creating.png)
+#
+
+# + [markdown] slideshow={"slide_type": "subslide"}
+# # Barriers to doing open science?
+# ![](../survey/figures/barriers.png)
+
+# + [markdown] slideshow={"slide_type": "subslide"}
+# #### If you do open science, 
+# # What kinds of resources did you find most useful?
+# ![](../survey/figures/wc_resources_that_helped.png)
+# -
+#
+
+# + [markdown] slideshow={"slide_type": "slide"}
+# # Milestones
+#
+# - Evaluate needs
+# - **Create documentation and website**
+# - Build network of collaborators
+# - Request funding
+
+# + [markdown] slideshow={"slide_type": "subslide"}
+# # GitHub
+# ![](images/github.png)
+# -
+
+# # Website
+
+# + [markdown] slideshow={"slide_type": "slide"}
+# # Milestones
+#
+# - Evaluate needs
+# - Create documentation and website
+# - **Build network of collaborators**
+# - Request funding
+
+# + [markdown] slideshow={"slide_type": "subslide"}
+# # Collaborators
+# ![](images/collaborators.png)
+
+# + [markdown] slideshow={"slide_type": "slide"}
+# # Milestones
+# - Evaluate needs
+# - Create documentation and website
+# - Build network of collaborators
+# - **Request funding**
+
+# + [markdown] slideshow={"slide_type": "subslide"}
+# # Why we need funding
+
+# + [markdown] slideshow={"slide_type": "subslide"}
+# # Proposed budget
+
+# + [markdown] slideshow={"slide_type": "subslide"}
+# # Potential funding opportunities
+
+# + [markdown] slideshow={"slide_type": "fragment"}
+# **Bold = already contacted**
+#
+# ### For overhead costs
+# - McGill PGSS Library Improvement Fund
+# - McGill Alumni Association student sponsorship program (\\$250 to \\$750)
+# - BrainHack School
+
+# + [markdown] slideshow={"slide_type": "fragment"} endofcell="--"
+# ### For collaborative events
+# - **GSAN**
+# - OSUM
+# - UNIQUE
+# - HBHL
+# - 
+# --
+
+# + [markdown] slideshow={"slide_type": "slide"}
+# # Next steps
+#
+# Developing my leadership and teaching skills
+# - ReproNim/INCF Training Fellowship ("Train-the-Trainor")
+# -
+
+
+
+# + [markdown] slideshow={"slide_type": "slide"}
+# # Acknowledgements
+#
+# Thanks to many people who helped create and share the survey, notably:
+# - Andrew Stewart (my OLS mentor)
+# - Samuel Guay (Open Sicence UMontreal)
+# - Samuel Burke (Open Sicence UMontreal)
+# - Pierre Bellec (BrainHack)
+# - Dylan Roskams-Edris (Tanenbaum Open Science Institute)
+# - My labmates
+# - John Oudyk
+#
+# -
+
+
+
