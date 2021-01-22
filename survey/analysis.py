0bd750f6fd5e160b1085b3f3549f1110cb98c64d
Kendra Oudyk <kendra.oudyk@gmail.com>
2021-01-22 16:27:22 -0500

    update survey analysis

--- survey/analysis.py
+++ survey/analysis.py
@@ -24,10 +24,13 @@ import textwrap
 
 import numpy as np
 import pandas as pd
+from scipy.optimize import brentq
 from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 import matplotlib.pyplot as plt
 import matplotlib
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Circle
 import seaborn as sns
 
 # %matplotlib inline
@@ -45,9 +48,9 @@ warnings.filterwarnings("ignore")
 # +
 survey = pd.read_csv("Doing Open Science in Grad School.csv", dtype=str)
 ##########################################
-non_pilot_data = np.arange(7, len(survey))
-survey = survey.iloc[non_pilot_data]
-survey.reset_index(drop=True, inplace=True)
+# non_pilot_data = np.arange(7, len(survey))
+# survey = survey.iloc[non_pilot_data]
+# survey.reset_index(drop=True, inplace=True)
 print("We have %d responses" % len(survey))
 
 pd.set_option("display.max_columns", None)
@@ -89,6 +92,8 @@ for col in list(cols.index):
 df.head(1)
 # -
 
+print('Number of participants: ', len(df))
+
 # ## Let's create regex expressions for categorizing answers
 #
 # There are a few reasons for doing this:
@@ -111,7 +116,7 @@ uni_regexs = {
 level_regexs = {
     "Undergrad": "[Uu]ndergrad|bac",
     "Masters": "[Mm]aster|[Mm]aitrise",
-    "Early PhD": "student|début",
+    "Early PhD": "student|début|[Bb][Ss]c",
     "Late PhD": "[Cc]andida",
     "Post-Doc": "([Pp]ost)([Dd]oc)",
     "Professor": "[Pp]rof",
@@ -297,15 +302,15 @@ sns.heatmap(
     mat.loc[create_keys, create_cols].T,
     annot=annot,
     vmax=vmax,
-    ax=axs[0],
+    ax=axs[1],
     cbar=cbar,
     cmap=cmap,
     cbar_kws={"label": "% of participants"},
 )
 #     cbar_kws={'label': 'Number of participants'})
 # axs[0].set_title("Experience CREATING open-science objects\n", fontsize=20)
-axs[0].set_yticklabels(ticklabels(create_cols))
-axs[0].set_xticklabels(ticklabels(create_keys), rotation=40, ha="right")
+axs[1].set_yticklabels(ticklabels(create_cols))
+axs[1].set_xticklabels(ticklabels(create_keys), rotation=40, ha="right")
 
 use_cols = [col for col in list(mat.columns) if "use_" in col]
 use_keys = ["dont_know", "never_used", "have_used", "would_like_to_use"]
@@ -313,32 +318,165 @@ sns.heatmap(
     mat.loc[use_keys, use_cols].T,
     annot=annot,
     vmax=vmax,
-    ax=axs[1],
+    ax=axs[0],
     cbar=cbar,
     cmap=cmap,
     cbar_kws={"label": "% of participants"},
 )
 #             cbar_kws={'label': 'Number of participants'})
 # axs[1].set_title("Experience USING open-science objects\n", fontsize=20)
-axs[1].set_yticklabels(ticklabels(use_cols))
-axs[1].set_xticklabels(ticklabels(use_keys), rotation=40, ha="right")
+axs[0].set_yticklabels(ticklabels(use_cols))
+axs[0].set_xticklabels(ticklabels(use_keys), rotation=40, ha="right")
 
 plt.tight_layout()
 extent = (
-    axs[0]
+    axs[1]
     .get_tightbbox(fig.canvas.renderer)
     .transformed(fig.dpi_scale_trans.inverted())
 )
 fig.savefig("figures/experience_creating.png", bbox_inches=extent)
 
 extent = (
-    axs[1]
+    axs[0]
     .get_tightbbox(fig.canvas.renderer)
     .transformed(fig.dpi_scale_trans.inverted())
 )
 fig.savefig("figures/experience_using.png", bbox_inches=extent)
 
 plt.savefig("figures/experience.png", bbox_inches="tight")
+# -
+
+# ## Overlap of creaters and users
+
+# +
+
+
+def intersection_area(d, R, r):
+    """Return the area of intersection of two circles.
+
+    The circles have radii R and r, and their centres are separated by d.
+
+    """
+
+    if d <= abs(R - r):
+        # One circle is entirely enclosed in the other.
+        return np.pi * min(R, r) ** 2
+    if d >= r + R:
+        # The circles don't overlap at all.
+        return 0
+
+    r2, R2, d2 = r ** 2, R ** 2, d ** 2
+    alpha = np.arccos((d2 + r2 - R2) / (2 * d * r))
+    beta = np.arccos((d2 + R2 - r2) / (2 * d * R))
+    return (
+        r2 * alpha
+        + R2 * beta
+        - 0.5 * (r2 * np.sin(2 * alpha) + R2 * np.sin(2 * beta))
+    )
+
+
+def find_d(A, R, r):
+    """
+    Find the distance between the centres of two circles giving overlap area A.
+
+    """
+    R = R + 0.0001
+    r = r + 0.0001
+
+    # A cannot be larger than the area of the smallest circle!
+    if A > np.pi * min(r, R) ** 2:
+        raise ValueError(
+            "Intersection area can't be larger than the area"
+            " of the smallest circle"
+        )
+    if A == 0:
+        # If the circles don't overlap, place them next to each other
+        return R + r
+
+    if A < 0:
+        raise ValueError("Negative intersection area")
+
+    def f(d, A, R, r):
+        return intersection_area(d, R, r) - A
+
+    a, b = abs(R - r), R + r
+    d = brentq(f, a, b, args=(A, R, r))
+    return d
+
+
+fig, axs = plt.subplots(3, 3, figsize=(14, 12))
+
+axs_nos = [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [1, 0],
+    [1, 1],
+    [1, 2],
+    [2, 0],
+    [2, 1],
+    [2, 2],
+]
+
+i_col = 0
+for use_col, create_col, [fig_x, fig_y] in zip(use_cols, create_cols, axs_nos):
+    i_used = df_cleaned[use_col].str.contains("have_used")
+    i_used = i_used.fillna(False)
+    n_used = i_used.sum()
+
+    i_created = df_cleaned[create_col].str.contains("have_created")
+    i_created = i_created.fillna(False)
+    n_created = i_created.sum() + 0.01
+
+    i_both = (i_used * 1) + (i_created * 1) > 1
+    n_both = i_both.sum()
+
+    # calculate distance between the two circles so that the
+    # overlap area is proportional to the intersection
+    overlap_area = n_both
+    radius_used = np.sqrt(n_used / np.pi)
+    radius_created = np.sqrt(n_created / np.pi)
+    dist_between_circles = find_d(overlap_area, radius_used, radius_created)
+
+    # Calculate the x coords that give the desired distance between the circles
+    y = -0.1
+    x_used = dist_between_circles - n_used / len(df)
+    x_created = dist_between_circles + x_used
+
+    # create the circles
+    circles = []
+    for x, r, color in zip(
+        [x_used, x_created], [radius_used, radius_created], ["green", "blue"]
+    ):
+        circles.append(Circle((x, y), r, linestyle="-"))
+
+    p = PatchCollection(circles, alpha=0.4, cmap=cmap)
+    p.set_clim([0, 10])
+    colors = np.array([6, 3])
+    p.set_array(colors)
+    axs[fig_x, fig_y].add_collection(p)
+    lim = 5
+    axs[fig_x, fig_y].set_ylim([-lim, lim - 1])
+    axs[fig_x, fig_y].set_xlim([-lim, lim])
+    axs[fig_x, fig_y].axis("off")
+
+    percent_used = int(n_used / len(df) * 100)
+    percent_created = int(n_created / len(df) * 100)
+    axs[fig_x, fig_y].annotate(
+        "%d%%\nUsed" % percent_used, xy=(-3.5, 2.8), fontsize=16, color="grey"
+    )
+    axs[fig_x, fig_y].annotate(
+        "%d%%\nCreated" % percent_created,
+        xy=(3.5, 2.8),
+        ha="right",
+        fontsize=16,
+        color="grey",
+    )
+
+    axs[fig_x, fig_y].set_title(use_col[4:].replace("_", " "), fontsize=20)
+
+plt.tight_layout()
+plt.savefig("figures/intersections_use_create.png", bbox_inches="tight")
 # -
 
 # ## Clustering types of participants
@@ -497,25 +635,25 @@ def plot_categorical_column_with_comments(
 
 
 # +
-fig, ax = plt.subplots(1, 1, figsize=(15, 9))
+fig, ax = plt.subplots(1, 1, figsize=(5, 5))
 plot_categorical_column_with_comments(
     column=df["barriers"],
     answer_options=barriers_options,
     ax=ax,
     title="What barriers do you face when trying to do open science?",
     plot_kind="barh",
-    print_other_comments=True,
+    print_other_comments=False,
 )
 fig.savefig("figures/barriers.png", bbox_inches="tight")
 
-fig, ax = plt.subplots(1, 1, figsize=(15, 9))
+fig, ax = plt.subplots(1, 1, figsize=(5, 3))
 plot_categorical_column_with_comments(
     df["resources_youd_like"],
     resources_options,
     ax=ax,
     title="What would help you make your research more open?",
     plot_kind="barh",
-    print_other_comments=True,
+    print_other_comments=False,
 )
 fig.savefig("figures/resources_youd_like.png", bbox_inches="tight")
 
@@ -537,8 +675,8 @@ for key, key_regex in host_options.items():
 host_mat[host_cols] = host_mat[host_cols].astype(int)
 
 # +
-matplotlib.rcParams.update({"font.size": 18})
-fig, ax = plt.subplots(figsize=(3, 3))
+matplotlib.rcParams.update({"font.size": 22})
+fig, ax = plt.subplots(figsize=(10, 5))
 
 sns.heatmap(
     host_mat.T,
@@ -549,11 +687,11 @@ sns.heatmap(
     cmap=cmap,
     cbar_kws={"label": "% of participants"},
 )
-ax.set_title(
-    "Would you be interested\nin applying to host\nOpen Science Office Hours?\n",
-    fontsize=20,
-)
-ax.set_yticklabels(["As a paid\nteaching assistant", "As a volunteer"])
+# ax.set_title(
+#     "Would you be interested\nin applying to host\nOpen Science Office Hours?\n",
+#     fontsize=20,
+# )
+ax.set_yticklabels(["As a paid\nteaching assistant", "As a volunteer"], rotation=0)
 plt.tight_layout()
 fig.savefig("figures/host.png", bbox_inches="tight")
 
@@ -563,7 +701,7 @@ fig.savefig("figures/host.png", bbox_inches="tight")
 # ## Open-ended questions
 
 # +
-def all_answers_wordcloud(column, title="", language="all"):
+def all_answers_wordcloud(column, title="", language="all", figsize=(20, 10)):
     if language == "en":
         column = column[df["language"] == "English"]
     elif language == "fr":
@@ -584,10 +722,10 @@ def all_answers_wordcloud(column, title="", language="all"):
         max_font_size=50, max_words=100, background_color="white"
     ).generate(all_texts)
 
-    fig, ax = plt.subplots(figsize=(20, 10))
+    fig, ax = plt.subplots(figsize=figsize)
     plt.imshow(wordcloud, interpolation="bilinear")
     plt.axis("off")
-    #     plt.title(title + "\n", fontsize=40)
+#     plt.title(title + "\n", fontsize=40)
     plt.show()
     return fig, ax
 
@@ -621,5 +759,186 @@ fig, ax = all_answers_wordcloud(
     df["resources_that_helped"],
     title="If you do open science,\nwhat kinds of resources did you find most useful\nfor learning how to do open science?",
     language=language,
+    figsize=(12, 8),
 )
 fig.savefig("figures/wc_resources_that_helped.png", bbox_inches="tight")
+# -
+
+# # Other analyses
+
+# ### Visualizing the hourly wages of TAs in Canada
+# First let's get the data in a dataframe
+
+# +
+ta_hourly_wages = {
+    "U of Toronto": 46,
+    "Ryerson U": 44,
+    "U of Ottawa": 43,
+    "U of Guelph": 42,
+    "Queens U": 39,
+    "York U": 37,
+    "Brock U": 33,
+    "U of Waterloo": 31,
+    "McGill U": 29,
+    "Concordia U": 28,
+    "Wilfred Laurier U": 28,
+    "McMaster U": 26,
+    "Simon Fraser U": 26,
+    "U of Victoria": 26,
+    "Carleton U": 25,
+    "U of Montreal": 23,
+    "Dalhousie U": 23,
+    "Polytechnique Montreal": 21,
+    "U of British Columbia": 20,
+    "U of Calgary": 19,
+    "U of Alberta": 18,
+}
+
+data = np.array(
+    [list(ta_hourly_wages.keys()), list(ta_hourly_wages.values())]
+).T
+ta_df = pd.DataFrame(data=data, columns=["University", "Hourly wage"])
+ta_df["Hourly wage"] = ta_df["Hourly wage"].astype(int)
+ta_df = ta_df.sort_values(by="Hourly wage")
+# -
+
+# Make the Montreal universities stand out in their color
+
+us_in_montreal = [
+    "McGill U",
+    "Polytechnique Montreal",
+    "Concordia U",
+    "U of Montreal",
+]
+colors = []
+for uni in list(ta_hourly_wages.keys()):
+    if uni in us_in_montreal:
+        color = "cornflowerblue"
+    else:
+        color = "lightgrey"
+    colors.append(color)
+colors = colors[::-1]
+
+# Aaaaand plot
+
+# +
+fig, ax = plt.subplots(figsize=(8, 8))
+
+plt.barh(ta_df["University"], ta_df["Hourly wage"], color=colors)
+# ta_df.plot(kind='barh', ax=ax, legend=False, color=['r', 'g'])
+x = ax.set_yticklabels(list(ta_df["University"]))
+ax.grid(axis="x")
+ax.set_xlabel("Hourly wage")
+ax.spines["right"].set_visible(False)
+ax.spines["top"].set_visible(False)
+plt.savefig("figures/ta_wages.png", bbox_inches="tight")
+# -
+
+# Mean and standard deviation of the TAs' hourly wages across Canada
+
+print(np.mean(ta_df["Hourly wage"]), np.std(ta_df["Hourly wage"]))
+
+# ## TA budget
+
+# +
+mean_ta_wage = 29.86
+std_ta_wage = 8.54
+possible_wages = np.arange(27, 32, 2)
+possible_n_osohs = np.array([24, 48])
+
+data = np.empty(shape=(len(possible_n_osohs), len(possible_wages)))
+wage_df = pd.DataFrame(columns=possible_wages, index=possible_n_osohs)
+n_tas = 2
+n_hrs_for_email = .5
+
+for n_osohs in possible_n_osohs:
+    for wage in possible_wages:
+        wage_df.at[n_osohs, wage] = np.round((n_tas * n_osohs * wage) + 
+                                             (n_tas * n_hrs_for_email * n_osohs * wage))
+        
+    
+
+fig, ax = plt.subplots(figsize=(10, 4))
+wage_df = wage_df.astype(int)
+sns.heatmap(
+    wage_df,
+    annot=True,
+    fmt="d",
+    ax=ax,
+    cmap=cmap,
+    cbar=False,
+#     cbar_kws={"label": "Yearly TA budget"},
+)
+
+ax.invert_yaxis()
+ylabels = [
+    "bi-weekly",
+    "weekly"
+]
+ax.set_yticklabels(ylabels, rotation=0)
+ax.set_ylabel("OSOH frequency")
+
+xlabels = ['$%d' %wage for wage in possible_wages]
+ax.set_xticklabels(xlabels, rotation=0)
+ax.set_xlabel("Hourly TA wage")
+
+ax.set_title(("- OSOH lasts 1 hour\n" +\
+              "- %d TAs host each OSOH\n" %n_tas +\
+              "- TAS do %.1f hr of email per OSOH\n" %n_hrs_for_email),
+            fontsize=18, loc='left')
+for t in ax.texts:
+    t.set_text("$" + t.get_text())
+plt.suptitle("Yearly TA budget", y=1.25)
+plt.savefig("figures/ta_budget.png", bbox_inches="tight")
+# +
+wage = 29.33
+
+possible_n_osohs = [24, 48]
+n_hrs_for_email = 1
+possible_hrs_per_week = [1, 1 + 1*n_hrs_for_email, 2, 2 + 2*n_hrs_for_email]
+
+data = np.empty(shape=(len(possible_hrs_per_week), len(possible_n_osohs)))
+wage_df2 = pd.DataFrame(columns=possible_n_osohs)
+
+for n_osohs in possible_n_osohs:
+    for i_n_hrs, n_hrs in enumerate(possible_hrs_per_week):
+        wage_df2.at[i_n_hrs, n_osohs] = np.round((n_osohs * n_hrs * wage))
+wage_df2 = wage_df2.T
+        
+fig, ax = plt.subplots(figsize=(15, 4))
+wage_df2 = wage_df2.astype(int)
+sns.heatmap(
+    wage_df2,
+    annot=True,
+    fmt="d",
+    ax=ax,
+#     cmap=cmap,
+    cbar=False,
+#     cbar_kws={"label": "Yearly TA budget"},
+)
+
+ax.invert_yaxis()
+ylabels = [
+    "bi-weekly",
+    "weekly"
+]
+ax.set_yticklabels(ylabels, rotation=0, fontsize=20)
+ax.set_ylabel("OSOH frequency\n")
+
+
+xlabels = [
+    "1 host",
+    "1 host\npaid for admin",
+    "2 hosts",
+    "2 hosts\npaid for admin"
+]
+ax.set_xticklabels(xlabels, rotation=0, fontsize=20)
+ax.set_xlabel("\nNumber of TAs hosting each OSOH\n& whether they're paid for admin work (%d hr/OSOH)" %n_hrs_for_email)
+
+for t in ax.texts:
+    t.set_text("$" + t.get_text())
+# plt.suptitle("TA budget projections", y=1.1)
+plt.savefig("figures/ta_budget.png", bbox_inches="tight")
+# -
+
+
